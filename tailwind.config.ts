e153a2bf85e654cfcf8eb4a512ae633def121c95
cup-code <xiangzhijiang37@gmail.com>
2025-08-01 14:24:11 +0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts,tsx}'],
+	theme: {}
+};
+
+export default config;
